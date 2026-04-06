7556558b14cfc0561c05da68e69b10ce04d8f979
Bijay Sharma <bijays2003@gmail.com>
2026-04-06 14:37:26 +0530

    commit

--- PaperUpload/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PaperUpload.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
